38ad0976b1887f0d9c2129d40a694b0c546dd235
Vivek Mudgal <mudgalv56@gmail.com>
2021-01-24 17:38:01 +0530

    FirstTime

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const HelloWorldApp = () => {
+  return(
+    <View style={{
+       flex:1,        //Fill item in available space.
+       justifyContent:'centre',         //align children of container in the centre of container's main axis.
+       alignItems:'center'              //align children of container in the centre of container's cross axis.
+    }}>
+       <Text>Hello</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default HelloWorldApp;
